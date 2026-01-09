52b12de48f8fe22de5754502a1f4363ff8b5c107
hanrrysantos <hanrry.jsantos@gmail.com>
2026-01-09 17:12:42 -0300

    Criando Projeto

---
+++ src/main/java/com/analise/curriculo/AnaliseDeCurriculoApplication.java
@@ -0,0 +1,13 @@
+package com.analise.curriculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDeCurriculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseDeCurriculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analise/curriculo/AnaliseDeCurriculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analise.curriculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseDeCurriculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
