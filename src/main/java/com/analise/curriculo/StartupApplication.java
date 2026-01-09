3230e204f6338c7ca8728fc3a5a1e057d15254db
hanrrysantos <hanrry.jsantos@gmail.com>
2026-01-09 17:15:32 -0300

    Alterando nome da classe principal

--- src/main/java/com/analise/curriculo/StartupApplication.java
+++ src/main/java/com/analise/curriculo/StartupApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnaliseDeCurriculoApplication {
+public class StartupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnaliseDeCurriculoApplication.class, args);
+		SpringApplication.run(StartupApplication.class, args);
 	}
 
 }
--- src/test/java/com/analise/curriculo/StartupApplicationTests.java
+++ src/test/java/com/analise/curriculo/StartupApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AnaliseDeCurriculoApplicationTests {
+class StartupApplicationTests {
 
 	@Test
 	void contextLoads() {
